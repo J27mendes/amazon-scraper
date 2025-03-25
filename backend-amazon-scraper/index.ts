513d8c0e30bdff0774f421ef6eaea8b844133684
J27mendez <mendes_j27@hotmail.com>
2025-03-25 20:52:50 -0300

    feat : Organization of data structures and auxiliary functions.

--- backend-amazon-scraper/index.ts
+++ backend-amazon-scraper/index.ts
@@ -1,7 +1,68 @@
-import express from "express";
+import axios from "axios";
+import express, { type Request, type Response } from "express";
+import { JSDOM } from "jsdom";
 
 const app = express();
 const port = 3000;
+
+interface Product {
+  title: string;
+  rating: string;
+  numReviews: string;
+  imageUrl: string;
+}
+
+// Function to extract information from Amazon
+const scrapeAmazon = async (keyword: string): Promise<Product[]> => {
+  try {
+    // URL for Amazon search with keyword
+    const url = `https://www.amazon.com.br/s?k=${encodeURIComponent(keyword)}`;
+
+    // Making the GET request with the 'User-Agent' header to avoid blocking
+    const response = await axios.get(url);
+
+    // Loading the response HTML with JSDOM
+    const dom = new JSDOM(response.data);
+    const products: Product[] = [];
+
+    // Getting all product listings on the page
+    dom.window.document
+      .querySelectorAll(".s-main-slot .s-result-item")
+      .forEach((item) => {
+        const title =
+          item.querySelector("h2 .a-link-normal")?.textContent || "Sem título";
+        const rating =
+          item.querySelector(".a-icon-alt")?.textContent || "Sem avaliação";
+        const numReviews =
+          item.querySelector(".s-link-style .a-size-base")?.textContent || "0";
+        const imageUrl =
+          item.querySelector(".s-image")?.getAttribute("src") || "";
+
+        products.push({
+          title,
+          rating,
+          numReviews,
+          imageUrl,
+        });
+      });
+
+    return products;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+// Route to start scraping
+app.get("/api/scrape", async (req: Request, res: Response) => {
+  const keyword = req.query.keyword as string;
+
+  // Getting the products with the scrapeAmazon function
+  const products = await scrapeAmazon(keyword);
+
+  // Returning the products as JSON
+  res.json(products);
+});
 
 // Iniciando o servidor na porta 3000
 app.listen(port, () => {
