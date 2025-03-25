8d3cc56575f9a52c42f57836cacebf29234ea825
J27mendez <mendes_j27@hotmail.com>
2025-03-25 20:42:30 -0300

    feat: Initial Express setup and port definition.

--- backend-amazon-scraper/index.ts
+++ backend-amazon-scraper/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello run Bun!");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// Iniciando o servidor na porta 3000
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
