7bda100d8e5510fc83e228d3c601a71f4e51a44f
J27mendez <mendes_j27@hotmail.com>
2025-03-26 15:52:22 -0300

    fix: add index.ts

--- backend-amazon-scraper/index.ts
+++ backend-amazon-scraper/index.ts
@@ -1,102 +1,10 @@
-import axios, { AxiosResponse } from "axios";
-import express, { Request, Response } from "express";
-import { JSDOM } from "jsdom";
-import cors from "cors";
+import app from "./src/app";
+import { config } from "dotenv";
 
-const app = express();
-const port = 3000;
+config();
 
-// Configure CORS (Cross-Origin Resource Sharing) on ​​an Express server
-app.use(cors({ origin: "http://localhost:5173" }));
+const port = process.env.PORT || 3000;
 
-// Typing the Product object
-interface Product {
-  title: string;
-  rating: string;
-  numReviews: string;
-  imageUrl: string;
-}
-
-// Function to extract information from Amazon
-const scrapeAmazon = async (keyword: string): Promise<Product[]> => {
-  try {
-    // URL for Amazon search with keyword
-    const url = `https://www.amazon.com.br/s?k=${encodeURIComponent(keyword)}`;
-
-    // Making the GET request with the 'User-Agent' header to avoid blocking
-    const response: AxiosResponse = await axios.get(url, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-      },
-    });
-
-    // Check if the response status is OK (200)
-    if (response.status !== 200) {
-      throw new Error(`Erro ao acessar a Amazon. Status: ${response.status}`);
-    }
-
-    // Loading the response HTML with JSDOM
-    const dom = new JSDOM(response.data);
-    const products: Product[] = [];
-
-    // Getting all product listings on the page
-    dom.window.document
-      .querySelectorAll(".s-main-slot .s-result-item")
-      .forEach((item) => {
-        const title =
-          item.querySelector("h2.a-size-base-plus span")?.textContent ||
-          "Sem título";
-        const rating =
-          item.querySelector(".a-icon-alt")?.textContent || "Sem avaliação";
-        const numReviews =
-          item.querySelector(".s-link-style .a-size-base")?.textContent || "0";
-        const imageUrl =
-          item.querySelector(".s-image")?.getAttribute("src") || "";
-
-        products.push({
-          title,
-          rating,
-          numReviews,
-          imageUrl,
-        });
-      });
-
-    return products;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
-
-// Route to start scraping
-app.get("/api/scrape", async (req: Request, res: Response) => {
-  const keyword = req.query.keyword as string;
-
-  // Checking if the keyword was passed and its length
-  if (!keyword || keyword.length < 2) {
-    return res
-      .status(400)
-      .json({ error: "A palavra-chave deve ter pelo menos 2 caracteres!" });
-  }
-
-  try {
-    // Getting the products with the scrapeAmazon function
-    const products = await scrapeAmazon(keyword);
-
-    // Returning the products as JSON
-    if (products.length === 0) {
-      return res.status(404).json({ message: "Nenhum produto encontrado." });
-    }
-
-    res.json(products);
-  } catch (error) {
-    // Returning a server error if something goes wrong
-    res.status(500).json({ error: "Erro ao buscar os produtos." });
-  }
-});
-
-// Starting the server on port 3000
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
